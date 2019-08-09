fc27e755c6fd5fbe7b53f0606e30bba42419b34c
Juior Dugue <jdugue@corp.kodner.com>
2019-08-09 11:52:22 -0400

    imported grid component from material ui and created structure base for my app via app.js component

--- src/App.js
+++ src/App.js
@@ -1,43 +1,31 @@
-import React, { useState } from "react";
-import { Grid } from "@material-ui/core";
+import React from "react";
 
-import { SearchBar, VideoList, VideoDetail } from "./components";
-
+import {Grid} from "@material-ui/core";
 import youtube from "./api/youtube";
+import {SearchBar, VideoList, VideoDetail} from "./components";
 
-export default () => {
-  const [videos, setVideos] = useState([]);
-  const [selectedVideo, setSelectedVideo] = useState(null);
-
-  return (
-    <Grid style={{ justifyContent: "center" }} container spacing={10}>
-      <Grid item xs={11}>
-        <Grid container spacing={10}>
-          <Grid item xs={12}>
-            <SearchBar onSubmit={handleSubmit} />
-          </Grid>
-          <Grid item xs={8}>
-            <VideoDetail video={selectedVideo} />
-          </Grid>
-          <Grid item xs={4}>
-            <VideoList videos={videos} onVideoSelect={setSelectedVideo} />
+class App extends React.Component{
+  render(){
+    return(
+      <Grid justify="center" container spacing={16}>
+        <h1>YouTube Clone App</h1>
+        <Grid item xs={12}>
+          <Grid container spacing={16}>
+            <Grid item xs={12}>
+            {/* Search Bar */}
+            
+            </Grid>
+            <Grid item xs={8}>
+              {/* Video Details */}
+            </Grid>
+            <Grid item xs={4}>
+              {/* Video Details */}
+            </Grid>
           </Grid>
         </Grid>
       </Grid>
-    </Grid>
-  );
-
-  async function handleSubmit(searchTerm) {
-    const { data: { items: videos } } = await youtube.get("search", {
-      params: {
-        part: "snippet",
-        maxResults: 5,
-        key: process.env.REACT_APP_API_KEY,
-        q: searchTerm,
-      }
-    });
-
-    setVideos(videos);
-    setSelectedVideo(videos[0]);
+    )
   }
 }
+
+export default App;
